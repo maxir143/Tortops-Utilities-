abe758537bc4fb168c174243dab8c1fadfb1709d
Your Name <you@example.com>
2022-03-04 18:33:53 -0600

    multiwindow fix

---
+++ autoclick.py
@@ -0,0 +1,135 @@
+import time
+import sys
+from pynput.mouse import Button, Controller
+from pynput import keyboard
+import threading
+import PySimpleGUI as sg
+
+
+class AutoClick:
+    def __init__(self):
+        self.mouse = Controller()
+        self.points_in_screen = []
+        self.state = 0
+        self.sound = 0
+        self.flag = 0
+        self.flags_state = []
+        self.layout = [[sg.Titlebar('Auto Click')],
+                       [sg.Text('<Ctrl> to record coordinate', key='display_text', font=("Helvetica", "10"))],
+                       [sg.Button('Start', key='btn_start', disabled=True, tooltip='Start/Stop <SHIFT>'), sg.Button('Erase', key='btn_erase', disabled=True), sg.Button('Reset', key='btn_reset', disabled=True), sg.Button('Quit')],
+                       [sg.Listbox([], size=(25, 5), enable_events=True, key='cords_list')],
+                       [[sg.Text('Record coordinate <CTRL> \rStart / Stop script <SHIFT>', key='info_text', font=("Helvetica", "8"), size=(25, 5))]]]
+        self.window = sg.Window('Auto Click', self.layout, size=(220, 200), disable_minimize=True, return_keyboard_events=True, use_default_focus=True)
+
+    def printInUi(self, txt=None):
+            try:
+                self.window['display_text'].update(txt)
+            except:
+                sys.exit()
+
+    def falseClick(self, iter):
+        for pos in self.points_in_screen:
+            should_continue = self.flags_state[iter]
+            if should_continue is False:
+                return
+            self.mouse.position = pos
+            self.mouse.press(Button.left)
+            self.mouse.release(Button.left)
+            self.printInUi('Clicked on: {0}'.format(pos))
+            time.sleep(1)
+        self.falseClick(iter)
+
+    def moveMousePosUi(self):
+        index = self.window["cords_list"].Widget.curselection()
+        if index:
+            index = index[0]
+            self.mouse.position = self.points_in_screen[index]
+
+    def recordClick(self):
+        if self.state == 0 or self.state == 2:
+            self.points_in_screen.append(self.mouse.position)
+            self.updateCordListUi(self.points_in_screen)
+            self.printInUi('Coord added: {}'.format(self.mouse.position))
+            self.focusUi()
+
+    def updateCordListUi(self, cords):
+        format_cord = []
+        for i in cords:
+            format_cord.append('x: {}, y: {}'.format(i[0], i[1]))
+        self.window["cords_list"].update(format_cord)
+        if cords:
+            self.window['btn_erase'].update(disabled=False)
+            self.window['btn_start'].update(disabled=False)
+            self.window['btn_reset'].update(disabled=False)
+            self.window['cords_list'].update(set_to_index=[len(self.points_in_screen) - 1], scroll_to_index=len(self.points_in_screen) - 1)
+        else:
+            self.window['btn_erase'].update(disabled=True)
+            self.window['btn_start'].update(disabled=True)
+            self.window['btn_reset'].update(disabled=True)
+            if self.state == 2:
+                self.state = 0
+
+    def focusUi(self):
+        self.window.BringToFront()
+
+    def eraseCordSelected(self, list="cords_list"):
+        if self.state == 0 or self.state == 2:
+            index = self.window[list].get()
+            if index:
+                index = self.window["cords_list"].GetIndexes()
+                self.printInUi('Coord deleted: {}'.format(self.points_in_screen[index[0]]))
+                self.points_in_screen.pop(index[0])
+                self.updateCordListUi(self.points_in_screen)
+
+    def startClick(self, message='Starting task'):
+        if self.state == 0:
+            if self.points_in_screen:
+                self.printInUi(message)
+                self.state = 1
+                self.flags_state.append(True)
+                self.flag = len(self.flags_state) - 1
+                threading.Thread(target=lambda:self.falseClick(self.flag),daemon=True).start()
+        elif self.state == 2:
+            self.state = 0
+            self.startClick('Resume')
+        elif self.state == 1:
+            self.state = 2
+            self.focusUi()
+            self.flags_state[self.flag] = False
+            self.printInUi('Pause')
+
+    def resetClick(self):
+        if self.state != 1:
+            self.state = 0
+            self.points_in_screen = []
+            if self.flags_state:
+                self.flags_state[self.flag] = False
+            self.updateCordListUi(self.points_in_screen)
+            self.printInUi('Coords reset successfully')
+
+    def run(self):
+        with keyboard.GlobalHotKeys({'<ctrl>': self.recordClick,'<shift>': self.startClick}) as s:
+            while True:
+                event, values = self.window.read()
+                # See if user wants to quit or self.window was closed
+                if event == 'btn_start':
+                    self.startClick()
+                elif event == 'btn_reset':
+                    self.resetClick()
+                elif event == 'btn_erase':
+                    self.eraseCordSelected()
+                elif event == 'cords_list':
+                    self.moveMousePosUi()
+                elif event == sg.WINDOW_CLOSED or event == 'Quit':
+                    break
+            self.window.close()
+
+    def start(self):
+        threading.Thread(target=self.run, daemon=True).start()
+
+
+'''
+QUICK START
+click = autoclick()
+click.run()
+'''
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from appdirs import user_data_dir
 
 
 def main():
+    """ RUN PROGRAM """
     """
         FUNCTIONS =====================================================================================
     """
@@ -56,71 +57,46 @@ def main():
                 _values.append(dict(_config.items(s)))
             return _values
 
-    def popup_error(title: str = 'Mandar Error', data: list = None, **kargs):
-        _data = ['', ''] if data is None else data
-        _layout = [[sg.Titlebar(title)],
-                   [sg.Text('Error:', size=5), sg.Multiline(_data[0], k='error')],
-                   [sg.Text('ID:', size=5), sg.InputText(_data[1], k='id', size=4), sg.Button('Submit', k='submit', expand_x=True)]]
-        return sg.Window('Mandar Error', _layout, keep_on_top=True, modal=True, **kargs).read(close=True)
+    def new_window(name: str, window: object, multiwindow:bool = False):
+        try:
+            WINDOWS[name].append(window)
+        except:
+            WINDOWS[name] = [window]
 
-    def popup_config(title: str = 'Configuracion', data: dict = None, **kargs):
+    def destroy_window(name, window):
+        WINDOWS[name].remove(window)
+        window.close()
+
+    def windows_bug(title: str = 'report_bug', **kargs):
+        _layout = [[sg.Titlebar(COMMANDS[title])],
+                   [sg.Text('Error:', size=5), sg.Multiline('', k='error')],
+                   [sg.Text('ID:', size=5), sg.InputText('', k='id', size=4), sg.Button('Submit', k='submit', expand_x=True)]]
+        _window = sg.Window(COMMANDS[title], _layout, keep_on_top=True, finalize=True, **kargs)
+        new_window(title, _window)
+
+    def windows_config(title: str = 'config', data: dict = None, **kargs):
         if data is None:
             return
-        _teleop_name = data['teleop_name']
-        _url = data['gsheet_url']
-        _json = data['json_file']
-        _page = data['gsheet_page']
-        _video_folder = data['rec_folder_path']
-        _fps = data['fps']
-        _record_urls = data['recording_urls']
-        _max_time = data['max_time_recording']
-
-        _layout = [[sg.Titlebar(title)],
+        _layout = [[sg.Titlebar(COMMANDS[title])],
                    [sg.Text('TELEOPERADOR')],
-                   [sg.Text('Nombre:', s=10), sg.InputText(_teleop_name, k='teleop_name', expand_x=True)],
+                   [sg.Text('Nombre:', s=10), sg.InputText(data['teleop_name'], k='teleop_name', expand_x=True)],
                    [sg.Text('_' * 64)],
                    [sg.Text('GOOGLE SHEET')],
-                   [sg.Text('URL:', s=10), sg.InputText(_url, k='gsheet_url', enable_events=True, expand_x=True)],
-                   [sg.Text('JSON:', s=10), sg.InputText(_json, k='json_file', disabled=True, enable_events=True), sg.FileBrowse(k='fb', file_types=(("JSON Files", "*.json"),), disabled=True)],
-                   [sg.Text('Pagina:', s=10), sg.InputCombo([_page], default_value=_page, k='gsheet_page', expand_x=True, disabled=True)],
+                   [sg.Text('URL:', s=10), sg.InputText(data['gsheet_url'], k='gsheet_url', enable_events=True, expand_x=True)],
+                   [sg.Text('JSON:', s=10), sg.InputText(data['json_file'], k='json_file', disabled=True, enable_events=True), sg.FileBrowse(k='fb', file_types=(("JSON Files", "*.json"),), disabled=True)],
+                   [sg.Text('Pagina:', s=10), sg.InputCombo([data['gsheet_page']], default_value=data['gsheet_page'], k='gsheet_page', expand_x=True, disabled=True)],
                    [sg.Text('_' * 64)],
                    [sg.Text('VIDEO')],
-                   [sg.Text('Carpeta:', s=10), sg.InputText(_video_folder, k='rec_folder_path', disabled=False, enable_events=True)],  #
-                   [sg.Text('Tiempo max:', s=10), sg.InputText(_max_time, k='max_time_recording', expand_x=True)],
-                   [sg.Text('FPS:', s=10), sg.InputCombo([1, 12, 15, 24, 30, 60], default_value=_fps, k='fps', expand_x=True)],
+                   [sg.Text('Carpeta:', s=10), sg.InputText(data['rec_folder_path'], k='rec_folder_path', disabled=False, enable_events=True)],  #
+                   [sg.Text('Tiempo max:', s=10), sg.InputText(data['max_time_recording'], k='max_time_recording', expand_x=True)],
+                   [sg.Text('FPS:', s=10), sg.InputCombo([1, 12, 15, 24, 30, 60], default_value=data['fps'], k='fps', expand_x=True)],
                    [sg.Text('_' * 64)],
                    [sg.Text('BUSCADOR')],
-                   [sg.Text('pagina(s):', s=10), sg.InputText(_record_urls, k='recording_urls', expand_x=True)],
-                   [sg.Button('Guardar', k='save', expand_x=True), sg.Button('Abrir Folder', k='open_folder'), sg.Button('Salir', k='exit', expand_x=True)]]
+                   [sg.Text('pagina(s):', s=10), sg.InputText(data['recording_urls'], k='recording_urls', expand_x=True)],
+                   [sg.Button('Guardar', k='save', expand_x=True), sg.Button('Abrir Folder', k='open_folder')]]
 
-        _window = sg.Window('Configuraciones globales', _layout, keep_on_top=True, modal=True, **kargs)
-        while True:
-            _event, _values = _window.read()
-            # print(_event, _values)
-            if _event == 'gsheet_url':
-                if validators.url(_values['gsheet_url']):
-                    _window.Element('fb').Update(disabled=False)
-                else:
-                    _window.Element('fb').Update(disabled=True)
-                    _window.Element('json_file').Update(value='')
-                    _window.Element('gsheet_page').Update(value='')
-            elif _event == 'json_file':
-                _sheet = SheetManager(_values['json_file'], _values['gsheet_url'])
-                if _sheet.check_connection():
-                    _window.Element('gsheet_page').Update(disabled=False)
-                    _sheets = _sheet.get_pages().keys()
-                    if _sheets:
-                        _window.Element('gsheet_page').Update(value=list(_sheets)[0], values=list(_sheets))
-                else:
-                    _window.Element('gsheet_page').Update(disabled=True, value='')
-            elif _event == 'save':
-                save_file(DATA['save_file'], DATA['config_section'], _values)
-                update_data(DATA, _values)
-            elif _event == 'open_folder':
-                os.startfile(DIR)
-            elif _event == sg.WINDOW_CLOSED or _event == 'Quit' or _event == 'exit':
-                break
-        _window.close()
+        _window = sg.Window(COMMANDS[title], _layout, keep_on_top=True, finalize=True, **kargs)
+        new_window(title, _window)
 
     def update_data(old_data: dict, new_data: dict):
         if old_data and new_data:
@@ -130,13 +106,9 @@ def main():
             return True
         return False
 
-    def start_autorecording():
-        threading.Thread(target=autorecording, daemon=True).start()
-
     def autorecording():
-        _time_out_time = 30
+        _time_out_time = 5
         _time_out = _time_out_time
-        _time_out = 5
 
         while True:
             time.sleep(1)
@@ -172,60 +144,104 @@ def main():
     """
     Program =====================================================================================
     """
-    DEBUG_CONSOLE = False
     DIR = user_data_dir('Utilities', 'Tortops', roaming=True)
     os.makedirs(DIR, exist_ok=True)
 
+    WINDOWS = {}
+
     DATA = {'teleop_name': 'Teleop',
             'gsheet_url': '',
-            'json_file': '',
+            'json_file': DIR,
             'gsheet_page': '',
             'rec_folder_path': DIR,
             'fps': 15,
             'recording_urls': 'teleoperation',
             'max_time_recording': 120,
-            'save_file': DIR + '\config.ini',
+            'save_file': f'{DIR}\config.ini',
             'config_section': 'Config'}
 
     if not update_data(DATA, read_file(DATA['save_file'], DATA['config_section'])):
-        popup_config(data=DATA)
+        windows_config(data=DATA)
+
+    # threading.Thread(target=autorecording, daemon=True).start()
 
     BROWSER = Browser()
     E = dt.now()
     RECORDER = Recorder(DATA['rec_folder_path'], DATA['fps'], int(DATA['max_time_recording']) * int(DATA['fps']) * 60)
-    COMMANDS = {'send_error': 'Reportar error', 'stop_recording': 'Detener grabacion', 'config': 'Configuracion', 'debug': 'Consola', 'exit': 'Salir'}
+    COMMANDS = {'report_bug': 'Reportar error', 'stop_recording': 'Detener grabacion', 'auto_click': 'Auto click', 'config': 'Configuracion', 'debug': 'Consola', 'exit': 'Salir'}
     COMMANDS_MENU = list(COMMANDS.values())
 
     LAYOUT = [[sg.Button('', image_filename=resource_path('tortoise.png'), image_size=(100, 100), border_width=0, button_color='white', right_click_menu=['&Right', COMMANDS_MENU])]]
     MAIN_WINDOW = sg.Window('Auto Click', LAYOUT, size=(100, 100), grab_anywhere=True, keep_on_top=True, alpha_channel=0.6, no_titlebar=True, transparent_color='white', element_padding=0, margins=(0, 0), finalize=True)
-    start_autorecording()
-    while True:
-        event, values = MAIN_WINDOW.read()
-        # print(event, values)
-        if event == COMMANDS['send_error']:
-            def send_error(title, data: list = None):
-                _g_sheet = SheetManager(DATA['json_file'], DATA['gsheet_url'])
-                if not _g_sheet.check_connection():
-                    return
-                pop = popup_error(title, data)
-                if pop[0]:
-                    _data = pop[1]
-                    if _data['error'] != '' and _data['id'] != '':
-                        _date = f'{E.day}/{E.month}/{E.year} {E.hour}:{E.minute}:{E.second}'
-                        _format_data = {'A': _data['id'], 'D': _data['error'], 'E': DATA['teleop_name'], 'F': _date}
-                        _g_sheet.send_report(DATA['gsheet_page'], _format_data)
-                    else:
-                        send_error('FALTA RELLENAR CAMPOS', [_data['error'], _data['id']])
+    WINDOWS['main'] = MAIN_WINDOW
 
-            send_error('Configuracion')
-        elif event == COMMANDS['config']:
-            popup_config(data=DATA)
-        elif event == COMMANDS['stop_recording']:
-            RECORDER.stop_recording()
-        elif event == COMMANDS['debug']:
-            print_window('[CONSOLA]')
-        elif event == sg.WINDOW_CLOSED or event == 'Quit' or event == COMMANDS['exit']:
-            break
+    while True:
+        window, event, values = sg.read_all_windows()
+        # print(f'Ventana: {window.Title}, Evento: {event}')
+        if window == WINDOWS['main']:
+            if event == COMMANDS['report_bug']:
+                windows_bug()
+            elif event == COMMANDS['config']:
+                windows_config(data=DATA)
+            elif event == COMMANDS['stop_recording']:
+                RECORDER.stop_recording()
+            elif event == COMMANDS['debug']:
+                print_window('[CONSOLA]')
+            elif event == COMMANDS['auto_click']:
+                pass
+            elif event == sg.WINDOW_CLOSED or event == 'Quit' or event == COMMANDS['exit']:
+                break
+
+        elif window.Title == COMMANDS['config']:
+            if event == 'gsheet_url':
+                if validators.url(values['gsheet_url']):
+                    window.Element('fb').Update(disabled=False)
+                else:
+                    window.Element('fb').Update(disabled=True)
+                    window.Element('json_file').Update(value='')
+                    window.Element('gsheet_page').Update(value='')
+            elif event == 'json_file':
+                sheet = SheetManager(values['json_file'], values['gsheet_url'])
+                if sheet.check_connection():
+                    window.Element('gsheet_page').Update(disabled=False)
+                    sheets = sheet.get_pages().keys()
+                    if sheets:
+                        window.Element('gsheet_page').Update(value=list(sheets)[0], values=list(sheets))
+                else:
+                    window.Element('gsheet_page').Update(disabled=True, value='')
+            elif event == 'save':
+                save_file(DATA['save_file'], DATA['config_section'], values)
+                update_data(DATA, values)
+                destroy_window('config', window)
+            elif event == 'open_folder':
+                os.startfile(DIR)
+            elif event == sg.WINDOW_CLOSED:
+                destroy_window('config', window)
+
+        elif window.Title == COMMANDS['report_bug']:
+            if event == 'submit':
+                g_sheet = SheetManager(DATA['json_file'], DATA['gsheet_url'])
+                if not g_sheet.check_connection():
+                    continue
+                if values['error'] != '' and values['id'] != '':
+                    date = f'{E.day}/{E.month}/{E.year} {E.hour}:{E.minute}:{E.second}'
+                    format_data = {'A': values['id'], 'D': values['error'], 'E': DATA['teleop_name'], 'F': date}
+                    g_sheet.send_report(DATA['gsheet_page'], format_data)
+                    destroy_window('report_bug', window)
+            elif event == sg.WINDOW_CLOSED:
+                destroy_window('report_bug', window)
+            '''def report_bug(title, data: list = None):
+                                _g_sheet = SheetManager(DATA['json_file'], DATA['gsheet_url'])
+                                if not _g_sheet.check_connection():
+                                    return
+                                pop = windows_bug(title, data)
+                                if pop[0]:
+                                    _data = pop[1]
+                                    
+                                    else:
+                                        report_bug('FALTA RELLENAR CAMPOS', [_data['error'], _data['id']])
+                            report_bug('Configuracion')'''
+
     MAIN_WINDOW.close()
 
 
